b55d5d71d7d999b1c404e9e0b1800c5e5cd61255
Dominicmburu <dominicmburu034@gmail.com>
2025-01-26 00:31:43 +0300

    route update

---
+++ src/api/main.ts
@@ -0,0 +1,1 @@
+export const API_BASE_URL = 'https://electronic-store-backend.onrender.com/api';
--- src/api/product.ts
+++ src/api/product.ts
@@ -1,11 +1,13 @@
+import { API_BASE_URL } from "./main";
+
 const productAPI = {
-    FEATURED_PRINTERS: "http://127.0.0.1:5000/api/products/featured",
-    LATEST_PRINTERS: "http://127.0.0.1:5000/api/products/latest",
-    ALL_PRODUCTS: (page: number, limit: number, _sortParam: string | undefined, _p0: string | undefined, _p1: string | undefined) => `http://127.0.0.1:5000/api/products?page=${page}&limit=${limit}`,
-    PRODUCT_DETAILS: (id: number | string): string => `http://127.0.0.1:5000/api/products/${id}`,
-    CREATE_PRODUCT: "http://127.0.0.1:5000/api/products/create",
-    UPDATE_PRODUCT: (id: number | string): string => `http://127.0.0.1:5000/api/products/${id}`,
-    DELETE_PRODUCT: (id: number | string): string => `http://127.0.0.1:5000/api/products/${id}`,
+    FEATURED_PRINTERS: `${API_BASE_URL}/products/featured`,
+    LATEST_PRINTERS: `${API_BASE_URL}/products/latest`,
+    ALL_PRODUCTS: (page: number, limit: number, _sortParam: string | undefined, _p0: string | undefined, _p1: string | undefined) => `${API_BASE_URL}/products?page=${page}&limit=${limit}`,
+    PRODUCT_DETAILS: (id: number | string): string => `${API_BASE_URL}/products/${id}`,
+    CREATE_PRODUCT: `${API_BASE_URL}/products/create`,
+    UPDATE_PRODUCT: (id: number | string): string => `${API_BASE_URL}/products/${id}`,
+    DELETE_PRODUCT: (id: number | string): string => `${API_BASE_URL}/products/${id}`,
 };
 
 export default productAPI;
